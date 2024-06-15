3a84f7fe556c3f4e3965ec81f6802209ff0cb72d
George Matos <georgemattos@gmail.com>
2024-06-15 16:42:51 -0300

    new session 06 added

---
+++ 06_PathParamsAndDealingWithExceptions/rest-with-spring-boot-and-java/src/test/java/com/mywallet/rest_with_spring_boot_and_java/RestWithSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywallet.rest_with_spring_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
